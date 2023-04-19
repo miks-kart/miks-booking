82ec0091a04666b0f31928cff67ab4d25f2dad1e
vradionovskii <vradionovskii@yandex.ru>
2023-04-19 15:33:30 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,11 @@ import {
   getOptimizedImage,
 } from "@components/image/imageFunctions";
 import { useStore } from "@components/Store";
-import { useLayoutEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { useLayoutEffect } from "react";
 // import PDFTest from "@components/PDFTest";
 
 export default function Index({ data, headerHeight }) {
   const initCart = useStore((state) => state.initCart);
-  const [isOpen, setIsOpen] = useState(false);
 
   let options = {
     extras: data.collapsibleOne.items,
@@ -57,59 +55,15 @@ export default function Index({ data, headerHeight }) {
           className="absolute top-0 left-0 w-screen h-auto pointer-events-none md:hidden"
         />
       </div>
-      <AnimatePresence>
-        {isOpen ? (
-          <section className="relative z-10 page-container  md:!pb-[26rem] !space-y-8 !pt-16">
-            <Hero data={data} />
-            <div className="space-y-[0.625rem] lg:space-y-8">
-              <CollapsibleOne headerHeight={headerHeight} data={data} />
-              <CollapsibleTwo headerHeight={headerHeight} data={data} />
-              <CollapsibleThree headerHeight={headerHeight} data={data} />
-            </div>
-            <Checkout data={data} />
-          </section>
-        ) : (
-          <motion.section
-            key="modal"
-            initial={{ opacity: 1 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="relative z-10 page-container md:!pb-20 !space-y-0 !pt-16"
-          >
-            <img
-              src={data.disc.icon}
-              className="mx-auto mb-10 w-44 h-44"
-              alt=""
-            />
-            <h2 className="pb-8 !text-4xl text-center text-white uppercase lg:text-5xl">
-              {data.disc.heading}
-            </h2>
-            <p className="max-w-3xl pb-10 !leading-none md:!leading-none mx-auto !text-sm  md:!text-2xl text-center text-white theme-text">
-              {data.disc.text}
-            </p>
-            <button
-              onClick={() => setIsOpen(true)}
-              className="mx-auto theme-button !table"
-            >
-              {data.disc.button}
-            </button>
-            <div className="grid grid-cols-2 gap-5 pt-20 lg:grid-cols-6">
-              {data.disc.items.map(({ item }) => (
-                <div className="" key={item.text}>
-                  <img
-                    src={item.image}
-                    alt={item.text}
-                    className="object-contain w-32 h-20 mx-auto lg:w-40 lg:h-28"
-                  />
-                  <p className="text-xs text-center text-white whitespace-pre-line">
-                    {item.text}
-                  </p>
-                </div>
-              ))}
-            </div>
-          </motion.section>
-        )}
-      </AnimatePresence>
+      <section className="relative z-10 page-container  md:!pb-[26rem] !space-y-8 !pt-16">
+        <Hero data={data} />
+        <div className="space-y-[0.625rem] lg:space-y-8">
+          <CollapsibleOne headerHeight={headerHeight} data={data} />
+          <CollapsibleTwo headerHeight={headerHeight} data={data} />
+          <CollapsibleThree headerHeight={headerHeight} data={data} />
+        </div>
+        <Checkout data={data} />
+      </section>
     </>
   );
 }
