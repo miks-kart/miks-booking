7e6744de25265e140ca7c95920d32465316cfde8
miks-kart <118816369+miks-kart@users.noreply.github.com>
2023-05-19 19:32:32 +0800

    Update sendToAdminFinal.js

--- pages/api/sendToAdminFinal.js
+++ pages/api/sendToAdminFinal.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
   const mailData = {
     from: '"MIKS KARTING" noreply.miks@gmail.com',
-    to: "manager@miks-kart.ru",
+    to: "manager@miks-karting.ru",
     subject: `Потверждение заказа`,
     text: `Заказ для ${req.body.name} подтвержден. Финальная смета находится в приложении к этому письму.`,
     html: `<p>Заказ для ${req.body.name} подтвержден. Финальная смета находится в приложении к этому письму.</p>`,
