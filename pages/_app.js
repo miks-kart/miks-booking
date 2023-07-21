f3dd51a50c32960a0536ee5b04f620c82455c6e2
miks-kart <118816369+miks-kart@users.noreply.github.com>
2023-07-21 18:44:09 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import { Rubik } from "next/font/google";
 import { useRef } from "react";
 import useSize from "@react-hook/size";
 import { ProgressiveImageSupportProvider } from "@components/image/ProgressiveImageSupportContext";
+import Script from "next/script";
 
 const rubik = Rubik({
   variable: "--font-rubik",
@@ -34,7 +35,7 @@ function App({ Component, pageProps }) {
         }}
       >
         <SmartOutline />
-
+        <Metrika />
         <SEO
           description={pageProps.data.description}
           title={pageProps.data.title}
@@ -52,5 +53,43 @@ function App({ Component, pageProps }) {
     </ProgressiveImageSupportProvider>
   );
 }
+
+function Metrika() {
+  return (
+
+      <>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/98E3"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+
+        <Script
+          id="gtm-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   
+   ym(98E3, "init", {
+     clickmap:true,
+     trackLinks:true,
+     accurateTrackBounce:true,
+     webvisor:true
+    });`,
+          }}
+        />
+      </>
+    
+  );
+}
+
 
 export default App;
