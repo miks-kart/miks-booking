5f9b257e20f4beb8bf402a64273f1e6ec44a2364
vradionovskii <82278782+vradionovskii@users.noreply.github.com>
2025-05-21 11:07:59 +0800

    Update GetItemTotalPrice.js

--- components/utils/GetItemTotalPrice.js
+++ components/utils/GetItemTotalPrice.js
@@ -2,6 +2,14 @@ export default function getItemTotalPrice(item) {
   return item.discountFrom?.enabled === "true" &&
     item.discountFrom?.discountPrice &&
     item.discountFrom?.from <= item.amount
-    ? item.amount * parseInt(item.discountFrom.discountPrice)
-    : item.amount * parseInt(item.price);
+    ? item.amount * parsePrice(item.discountFrom.discountPrice)
+    : item.amount * parsePrice(item.price);
+}
+
+function parsePrice(price) {
+   let parsedPrice = parseInt(price);
+   if (isNaN(parsedPrice)) {
+    parsedPrice = 0;
+   }
+   return parsedPrice;
 }
